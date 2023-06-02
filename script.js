5ac8101071e01dff41f5277a54ffb171a229ebae
rakurakuraku <127064071+rakurakuraku@users.noreply.github.com>
2023-06-02 11:30:29 +0800

    Add initial pseudocode outlining game / needs

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-//
+//define possible choices (R, P, S)
+    //each one a variable?
+    //can you make variables < or > (as if they were numbers?)
+//define how each choice interacts with the others
+//determine player choice
+//determine computer choice
+    //how to make it random?
+//determine who wins
+//communicate result to player
+
+//later
+//assign score for each round
+//keep score (up to 5)
+//declare winner at end of game (5 rounds)
