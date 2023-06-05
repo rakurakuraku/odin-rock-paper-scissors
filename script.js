08bd35e683f0efb61f34bbe94d2d06f1c73dd58e
rakurakuraku <127064071+rakurakuraku@users.noreply.github.com>
2023-06-05 15:49:36 +0800

    Add basic working single round game

--- script.js
+++ script.js
@@ -7,13 +7,56 @@
 //determine computer choice
     //how to make it random?
         //pick a number, 0 = rock, 1 = paper 2 = scissors
-function randomNumber() {
-    let computerChoice; 
-    if { Math.floor(Math.random() * 3) === 0
-}
+
+let cpuChoice = '';
+let playerChoice = '';
+let message = '';
+
+function computerChoice() { 
+    if (Math.floor(Math.random() * 3) === 0) {
+        cpuChoice = 'rock';}
+    if (Math.floor(Math.random() * 3) === 1) {
+        cpuChoice = 'paper';}
+    if (Math.floor(Math.random() * 3) === 2) {
+        cpuChoice = 'scissors';}
+    return(cpuChoice);
+    }
+
 //determine outcome
+
+
+
+function round(playerChoice, cpuChoice) {
+    if ((playerChoice === 'rock') && (cpuChoice === 'scissors') ||
+        (playerChoice === 'paper') && (cpuChoice === 'rock') ||
+        (playerChoice === 'scissors') && (cpuChoice === 'paper')) {
+//        playerScore = (playerScore + 1);
+//        cpuScore = (cpuScore + 0);
+        message = 'You Win!' /* + [playerChoice] + beats + [cpuChoice]*/;
+    }
+    if ((cpuChoice === 'rock') && (playerChoice === 'scissors') ||
+        (cpuChoice === 'paper') && (playerChoice === 'rock') ||
+        (cpuChoice === 'scissors') && (playerChoice === 'paper')) {
+//        playerScore = (playerScore + 0);
+//        cpuScore = (cpuScore + 1);
+        message = 'You Lose!' /* + [cpuChoice] + beats + [playerChoice]'*/;
+    }
+    if (playerChoice === cpuChoice) {
+        message = 'Draw!'
+    }
+    console.log(message);
+}
+
+//    
+//
+
+
     //win, lose, draw?
+    //compare player and cpu choices and give winner (rules are in function)
+    //could break into 'you win' and 'they win' scenarios
     //what are the rules for a draw? 0 points? re-play?
+    //both choices should be rendered to all lowercase internally.
+    //if playerChoice === cpuChoice result = draw (and points = XX)
 //communicate result to player
 
 //later
